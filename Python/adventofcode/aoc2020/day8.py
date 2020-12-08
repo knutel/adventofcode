5fd21f72440c9b447e8fcac3b9ce0734e615087c
Knut Eldhuset <knut.eldhuset@gmail.com>
2020-12-08 06:39:40 +0100

    Moves CPU implementation to a class to make future changes simpler

--- Python/adventofcode/aoc2020/day8.py
+++ Python/adventofcode/aoc2020/day8.py
@@ -2,6 +2,34 @@ from collections import namedtuple
 
 Instruction = namedtuple("Instruction", "opcode, operand")
 
+
+class SantaClaws:
+    def __init__(self, program):
+        self.program = program
+        self.pc = 0
+        self.acc = 0
+        self.halted = False
+
+        self.last_address = len(self.program) - 1
+
+    def step(self):
+        if self.pc < 0 or self.pc > self.last_address:
+            self.halted = True
+            return
+        instruction = self.program[self.pc]
+        if instruction.opcode == "acc":
+            self.acc += instruction.operand
+            self.pc += 1
+        elif instruction.opcode == "jmp":
+            self.pc += instruction.operand
+        elif instruction.opcode == "nop":
+            self.pc += 1
+
+    def run(self):
+        while not self.halted:
+            self.step()
+
+
 def parse_program(lines):
     program = []
     for line in lines:
@@ -11,26 +39,19 @@ def parse_program(lines):
             program.append(Instruction(opcode, operand))
     return program
 
-def run_program(program):
-    pc = 0
-    acc = 0
+
+def run_program_with_loop_detection(program):
+    cpu = SantaClaws(program)
     visited = set()
-    while pc not in visited and pc < len(program):
-        instruction = program[pc]
-        visited.add(pc)
-        if instruction.opcode == "acc":
-            acc += instruction.operand
-            pc += 1
-        elif instruction.opcode == "jmp":
-            pc += instruction.operand
-        elif instruction.opcode == "nop":
-            pc += 1
-    return pc, acc
+    while cpu.pc not in visited and not cpu.halted:
+        visited.add(cpu.pc)
+        cpu.step()
+    return cpu.pc, cpu.acc
 
 
 def part1(lines):
     program = parse_program(lines)
-    (pc, acc) = run_program(program)
+    (pc, acc) = run_program_with_loop_detection(program)
     return acc
 
 
@@ -44,8 +65,8 @@ def part2(lines):
             mutated_program[mutate_instruction] = Instruction("nop", instruction.operand)
         elif instruction.opcode == "nop":
             mutated_program[mutate_instruction] = Instruction("jmp", instruction.operand)
-        (pc, acc) = run_program(mutated_program)
-        if (pc > len(mutated_program) - 1):
+        (pc, acc) = run_program_with_loop_detection(mutated_program)
+        if pc > len(mutated_program) - 1:
             return acc
     return None
 
